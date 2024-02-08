5007244bfcfcda0fd65c45c0e1159f77a9b97a56
AntonioHollerman <toniohollerProf@gmail.com>
2024-02-08 15:38:02 -0500

    create initial structure of items.pyCrawler

--- AnimeCrawlers/AnimeCrawlers/items.py
+++ AnimeCrawlers/AnimeCrawlers/items.py
@@ -4,9 +4,23 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+hi = ['anime_id', 'name', 'description', 'rating', 'studio', 'themes',
+      'categories', 'eps', 'mins_per_epi']
 
 
-class AnimecrawlersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class InfoCrawlerItem(scrapy.Item):
+    title = scrapy.Field()
+    description = scrapy.Field()
+    rating = scrapy.Field()
+    studio = scrapy.Field()
+    themes = scrapy.Field()
+    categories = scrapy.Field()
+    eps = scrapy.Field()
+    mins_per_epi = scrapy.Field()
+
+
+class DescCrawlerItem(scrapy.Item):
+    name = scrapy.Field()
+    username = scrapy.Field()
+    recommendation = scrapy.Field()
+    review = scrapy.Field()
