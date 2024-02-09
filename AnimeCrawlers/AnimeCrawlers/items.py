5619f1eb15f2d10627c64244f4b5381e9c7639a9
AntonioHollerman <toniohollerProf@gmail.com>
2024-02-09 11:40:18 -0500

    First draft of DescCrawler loaders

--- AnimeCrawlers/AnimeCrawlers/items.py
+++ AnimeCrawlers/AnimeCrawlers/items.py
@@ -4,21 +4,63 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+
+def clear_white_space(string: str):
+    return string.strip().replace("  ", "").replace("\n", "")
 
 
 class InfoCrawlerItem(scrapy.Item):
-    title = scrapy.Field()
-    description = scrapy.Field()
-    rating = scrapy.Field()
-    studio = scrapy.Field()
-    themes = scrapy.Field()
-    categories = scrapy.Field()
-    eps = scrapy.Field()
-    mins_per_epi = scrapy.Field()
+    title = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    description = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    rating = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    studio = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    themes = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    categories = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    eps = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
+    mins_per_epi = scrapy.Field(
+        input_processor=MapCompose(),
+        output_processor=TakeFirst()
+    )
 
 
 class DescCrawlerItem(scrapy.Item):
-    title = scrapy.Field()
-    username = scrapy.Field()
-    recommendation = scrapy.Field()
-    review = scrapy.Field()
+    title = scrapy.Field(
+        input_processor=MapCompose(remove_tags, clear_white_space),
+        output_processor=TakeFirst()
+    )
+    username = scrapy.Field(
+        input_processor=MapCompose(remove_tags, clear_white_space),
+        output_processor=TakeFirst()
+    )
+    recommendation = scrapy.Field(
+        input_processor=MapCompose(remove_tags, clear_white_space),
+        output_processor=TakeFirst()
+    )
+    review = scrapy.Field(
+        input_processor=MapCompose(remove_tags, clear_white_space),
+        output_processor=TakeFirst()
+    )
