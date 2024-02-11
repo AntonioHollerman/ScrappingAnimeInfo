099d4b4f427c73936a7ae91fc54188c3d7f4a61b
Antonio Hollerman <toniohollerprof@gmail.com>
2024-02-11 15:03:50 -0500

    Finish loading for anime info

--- AnimeCrawlers/AnimeCrawlers/items.py
+++ AnimeCrawlers/AnimeCrawlers/items.py
@@ -6,44 +6,96 @@
 import scrapy
 from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
+import numpy as np
+from selectolax.parser import HTMLParser
 
 
 def clear_white_space(string: str):
     return string.strip().replace("  ", "").replace("\n", "")
 
 
+def return_float(string: str):
+    try:
+        return float(string)
+    except ValueError:
+        return np.nan
+
+
+def extract_themes(values: list):
+    for node in values:
+        if "Theme" in remove_tags(node):
+            div = HTMLParser(node)
+            themes = [a.text() for a in div.css('a')]
+            if not themes:
+                return np.nan
+            return ', '.join(themes)
+
+    return np.nan
+
+
+def extract_studio(values: list):
+    for node in values:
+        if "Studio" in remove_tags(node):
+            div = HTMLParser(node)
+            a_tag = div.css_first('a')
+            if a_tag is None:
+                return np.nan
+            return a_tag.text()
+
+    return np.nan
+
+
+def extract_categories(string: str):
+    node = HTMLParser(string)
+    categories = [a.text() for a in node.css('a')]
+    return ", ".join(categories)
+
+
+def extract_eps(string: str):
+    eps = ''.join([val for val in string if val.isdigit()])
+    try:
+        return int(eps)
+    except ValueError:
+        return np.nan
+
+
+def take_second(values):
+    for val in values[1:]:
+        if isinstance(val, int):
+            return val
+    return np.nan
+
+
 class InfoCrawlerItem(scrapy.Item):
     title = scrapy.Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose(remove_tags, clear_white_space),
         output_processor=TakeFirst()
     )
     description = scrapy.Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose(remove_tags, clear_white_space),
         output_processor=TakeFirst()
     )
     rating = scrapy.Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose(remove_tags, clear_white_space, return_float),
         output_processor=TakeFirst()
     )
     studio = scrapy.Field(
-        input_processor=MapCompose(),
-        output_processor=TakeFirst()
+        output_processor=extract_studio
     )
     themes = scrapy.Field(
-        input_processor=MapCompose(),
-        output_processor=TakeFirst()
+        output_processor=extract_themes
     )
     categories = scrapy.Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose(extract_categories),
         output_processor=TakeFirst()
     )
     eps = scrapy.Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose(remove_tags, extract_eps),
         output_processor=TakeFirst()
     )
     mins_per_epi = scrapy.Field(
-        input_processor=MapCompose(),
-        output_processor=TakeFirst()
+        input_processor=MapCompose(remove_tags, extract_eps),
+        output_processor=take_second
     )
 
 
