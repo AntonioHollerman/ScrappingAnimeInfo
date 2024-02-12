39a1894e218fc83317f7aedc7de87f3f029630f8
Antonio Hollerman <toniohollerprof@gmail.com>
2024-02-12 18:40:22 -0500

    Fix database connection issue. Do this by setting connection to autocommit=True

--- AnimeCrawlers/AnimeCrawlers/pipelines.py
+++ AnimeCrawlers/AnimeCrawlers/pipelines.py
@@ -5,7 +5,10 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
+import sys
+sys.path.append('../')
+from db_funcs import *
 
 
 class FilterPipeline:
@@ -13,15 +16,36 @@ class FilterPipeline:
         return item
 
 
-class SaveToDb:
+class InfoToDb:
     def __init__(self):
-        pass
+        db_conn.set_session(autocommit=True)
 
     def open_spider(self, spider):
-        pass
+        create_tables()
 
     def process_item(self, item, spider):
+        row = InfoRow(find_info_id(item['title']), item['title'], item['description'], item['rating'], item['studio'],
+                      item['themes'], item['categories'], item['eps'], item['mins_per_epi'])
+        insert_info_row(row)
         return item
 
     def close_spider(self, spider):
-        pass
+        db_cur.close()
+        db_conn.close()
+
+
+class DescToDb:
+    def __init__(self):
+        db_conn.set_session(autocommit=True)
+
+    def open_spider(self, spider):
+        create_tables()
+
+    def process_item(self, item, spider):
+        row = ReviewRow(find_info_id(item["title"]), item['username'], item['recommendation'], item['review'])
+        insert_review_row(row)
+        return item
+
+    def close_spider(self, spider):
+        db_cur.close()
+        db_conn.close()
